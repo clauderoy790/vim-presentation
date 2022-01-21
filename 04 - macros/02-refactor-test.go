0ffa7c69b8389d17d57d41bd5ac38f84671b10d8
claude_roy <Claude.Roy@mheducation.com>
2022-01-21 10:30:39 -0600

    add macros

---
+++ 04 - macros/02-refactor-test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type testSuite struct {
+	suite.Suite
+}
+
+func TestOne(t *testing.T) {
+
+}
+
+func TestTwo(t *testing.T) {
+
+}
+
+func TestThree(t *testing.T) {
+
+}
+
+func TestFour(t *testing.T) {
+
+}
+
+func TestFive(t *testing.T) {
+
+}
